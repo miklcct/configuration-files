7808ceeb0237b11a3af62967da8789309381e74b
Michael Tsang <miklcct@gmail.com>
2017-05-25 14:41:14 +0800

    improve install script

--- install.sh
+++ install.sh
@@ -5,14 +5,16 @@ do
     if
         [ -f "$file" ] && git ls-files "$file" --error-unmatch >/dev/null 2>&1
     then
-        echo Please enter one of the following for $file
-        echo copy : Copy the file to the home directory, overwriting it.
-        echo skip : Do not copy this configuration file.
-        echo diff : Launch vimdiff to interactively edit the file.
         processed=
         while
             [ -z "$processed" ]
         do
+            echo
+            echo Please enter one of the following for $file
+            echo copy : Copy the file to the home directory, overwriting it.
+            echo skip : Do not copy this configuration file.
+            echo diff : Launch vimdiff to interactively edit the file. You will \
+                have a chance to enter the response again.
             read command
             case "$command" in
                 copy)
@@ -24,7 +26,6 @@ do
                     ;;
                 diff)
                     vimdiff ~/"$file" "$file"
-                    processed=true
                     ;;
                 *)
                     echo Invalid response. Please enter again.
