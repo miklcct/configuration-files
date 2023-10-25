ea8dbe0c681a9b2c0a354d6a227391348c691da8
Michael Tsang <miklcct@gmail.com>
2023-10-25 09:54:14 +0100

    set fish variables in config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,10 @@
+set __fish_git_prompt_describe_style branch
+set __fish_git_prompt_showcolorhints 1
+set __fish_git_prompt_showdirtystate 1
+set __fish_git_prompt_showstashstate 1
+set __fish_git_prompt_showupstream auto
+set fish_prompt_pwd_dir_length 0
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
