95cfff30d50808d3abb05e8686a934f947471cba
Michael Tsang <michael.tsang@jnction.co.uk>
2023-12-05 12:37:33 +0000

    set bold flag on remote host prompt

--- .config/fish/functions/prompt_login.fish
+++ .config/fish/functions/prompt_login.fish
@@ -11,6 +11,7 @@ function prompt_login --description 'display user name for the prompt'
         set color_host $fish_color_host_root
     end
     if set -q SSH_TTY; and set -q fish_color_host_remote
+        set bold_flag --bold
         set color_host $fish_color_host_remote
         if test $__is_root = true
             if set -q fish_color_host_remote_root
@@ -19,5 +20,5 @@ function prompt_login --description 'display user name for the prompt'
         end
     end
 
-    echo -n -s (set_color $fish_color_user) "$USER" (set_color $initial) @ (set_color $color_host) (prompt_hostname) (set_color $initial)
+    echo -n -s (set_color $fish_color_user) "$USER" (set_color $initial) @ (set_color $bold_flag $color_host) (prompt_hostname) (set_color $initial)
 end
